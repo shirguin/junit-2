df093be2cdac9e311a2b2c2831f9a3f6f7e0704b
Shirguin <shirguin@yandex.ru>
2023-09-13 19:57:42 +0300

    Initial commit

---
+++ src/test/java/hw/VehicleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VehicleTest {
+    private Car car;
+    @BeforeEach
+    public void setUp() {
+
+    }
+}
